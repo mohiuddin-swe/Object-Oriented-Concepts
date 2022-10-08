6fd6d287733bfe0b5082ec2174c4e94d21cd496c
Mohiuddin <101384674+mohiuddin-swe@users.noreply.github.com>
2022-10-08 19:57:02 +0600

    array

--- 19 Array/array.java
+++ 19 Array/array.java
@@ -1,40 +1,41 @@
-//Java Arrays
+public class array{
+  public static void main(String[] args){
+    
+    //Array initialize and assigning value
+    String cars[]= {"Volvo", "BMW", "Ford", "Mazda"};
 
-    String[] cars; //Delclare Character array or Strings
-    
-    //Assigning value
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    
-    int[] myNum = {10, 20, 30, 40}; //Number Array
-    
-    //Accessing element
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     System.out.println(cars[0]);  // Outputs Volvo
-   
-    
-    //Change an Array Element
-    
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
     cars[0] = "Opel";
-    System.out.println(cars[0]);// Now outputs Opel instead of Volvo
+    System.out.println(cars[0]); // Now outputs Opel instead of Volvo
     
-    
-    /* //Array length
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     System.out.println(cars.length);
-    // Outputs 4
-     */
-    /* //Array loop/ Traverse 
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+   
+   //Array loop/ Traverse 
+    System.out.println("\nFor loop Started from here");
     for (int i = 0; i < cars.length; i++) {
       System.out.println(cars[i]);
     }
-     */
-    
-   /*  //Traverse using for each loop
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+     
+   //Traverse using for each loop
+   System.out.println("\nFor Each loop Started from here");
     for (String i : cars) {
       System.out.println(i);
     }
-    */
+  
+ }
+}
+
+
+    
+    
+   
+  
+    
+    
+  
+    
+     
+   
+    
     
